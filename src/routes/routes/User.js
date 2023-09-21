b65b6c05384f192d201e775672464953f5adfdb5
raynerb300 <raynerb3000@gmail.com>
2023-09-20 22:46:24 -0400

    feat(Routes): Implementacion de las rutas privadas

--- src/routes/routes/User.js
+++ src/routes/routes/User.js
@@ -1,14 +1,15 @@
 import UserController from "../../controllers/UserController.js";
 import { URL } from "../../helpers/Url.js";
+import { authentication } from "../../middlewares/Auth.js";
 
 // Definición de rutas del modulo de usuario
 const routesUser = (router) =>{
-    router.get(URL.getUsers, UserController);
-    router.get(URL.getUser, UserController);
-    router.post(URL.createUser, UserController);
-    router.patch(URL.updateUser, UserController);
-    router.patch(URL.resetPasswordUser, UserController);
-    router.patch(URL.changePassword, UserController);
+    router.get(URL.getUsers, authentication, UserController);
+    router.get(URL.getUser, authentication, UserController);
+    router.post(URL.createUser, authentication, UserController);
+    router.patch(URL.updateUser, authentication, UserController);
+    router.patch(URL.resetPasswordUser, authentication, UserController);
+    router.patch(URL.changePassword, authentication, UserController);
 }
 
 export default routesUser; 
